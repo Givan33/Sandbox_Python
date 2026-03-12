2f085eecb14f1fc83236ebe704e83ab52cc5fb7c
Givan33 <igaspar33@gmail.com>
2026-03-12 16:48:53 -0600

    Primer commit para esto de python

---
+++ Hola_Mundo.py
@@ -0,0 +1,40 @@
+for i in range(0,5):
+    print(i,"hola")
+
+myBoolena = True
+if myBoolena:
+    print("Verdad")
+
+# this is a comment
+
+#Potencia de un numero
+power = 4**4
+print(power)
+
+#Division entera
+division_entera = 16//5
+print(division_entera)
+
+#Operacion Modulo
+modulo = 7%2
+print(modulo)
+
+#Operadores de asignacion
+numero1 = 5
+numero1 +=7
+print(numero1)
+
+# ejemplo con Print()
+print((3-2)*5+(2**4)/16)
+
+# ejemplo de como evitar el error en el calculo con numeros flotantes
+ex1 = 1.23 + 2.80
+print("Con error flotante:", ex1)
+ex2 = (123 + 280) / 100
+print("Sin error flotante:", ex2)
+ex3 = 1.23 + 2.80
+print("Sin error usando round():",round(ex3,2))
+print((1090 + 21105)/100)
+
+
+#aqui ponemos otro comentaio y borramos el anterior
